ecb9a18d293c92dd35f09b67b281dc6c801f163e
Djayos <joseph.beaumont1@gmail.com>
2021-01-29 23:39:25 +0100

    add sprites
    added new sprite for the main character

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import random
 
 pygame.init()
 
+JumpRight = [pygame.image.load('saut_normal_droit_1.png'), pygame.image.load('saut_normal_droit_2.png'), pygame.image.load('saut_normal_droit_3.png'), pygame.image.load('saut_normal_droit_4.png'), pygame.image.load('saut_normal_droit_5.png')]
+JumpLeft = [pygame.image.load('saut_normal_gauche_1.png'), pygame.image.load('saut_normal_gauche_2.png'), pygame.image.load('saut_normal_gauche_3.png'), pygame.image.load('saut_normal_gauche_4.png'), pygame.image.load('saut_normal_gauche_5.png')]
+
+
 run = True
 while run:
     pygame.time.delay(27)
@@ -22,20 +26,23 @@ while run:
     if keys[pygame.K_RIGHT] and x < 1120:
         x += vel
         perso = pygame.image.load('perso jeu droite.png')
+
     
 
     if y >= 600:              #position du perso
         vely = 0              #si dans le sol => remonte a 600
         if y > 600:           #si sur sol vely=0 car pas de gravité
             y = 600
+            
+
     else :
         if keys[pygame.K_DOWN]:
             vely = min(vely + 10, 100) 
         else :
             vely = min(vely + 3, 100)      #ajout de la gravité
 
-    if keys[pygame.K_SPACE]:
-        vely = -20                  #saut si espace appuyé
+    if keys[pygame.K_SPACE]:                #saut si espace appuyé
+        vely = -20
 
     window.blit(fond, (0, 0))
     window.blit(perso, (x, y))
