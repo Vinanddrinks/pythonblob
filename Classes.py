ade9a82c2e3434323323c52ac3d357ab00278b1c
Zbram <75671598+Zbram@users.noreply.github.com>
2021-04-13 23:31:01 +0200

    create functions to import sprites
    incomplete

--- Classes.py
+++ Classes.py
@@ -7,6 +7,7 @@ import pygame as pg
 import numpy as n
 import time as t
 import random as r
+from functions import *
 # End-importation
 
 #Begin
@@ -25,8 +26,79 @@ class player_entity:
         self.Ang = 398503743
         
         #status
-        self.health = 5
+        self.health = 3
         self.bonus = 0
+
+        #all sprites
+        self.sprites1_right = []
+        self.sprites1_left = []
+        self.sprites2_right = []
+        self.sprites2_left = []
+        self.sprites3_right = []
+        self.sprites3_left = []
+        self.sprites4 = []
+        
+        #SPRITE 1
+        self.sprites1_right.append(pg.image.load('.\resources\normal\saut slime normal droit\saut_normal_droit_0.png'))
+        self.sprites1_right.append(pg.image.load('\resources\normal\saut slime normal droit\saut_normal_droit_1.png'))
+        self.sprites1_right.append(pg.image.load('\resources\normal\saut slime normal droit\saut_normal_droit_2.png'))
+        self.sprites1_right.append(pg.image.load('\resources\normal\saut slime normal droit\saut_normal_droit_3.png'))
+        self.sprites1_right.append(pg.image.load('\resources\normal\saut slime normal droit\saut_normal_droit_4.png'))
+        self.sprites1_right.append(pg.image.load('\resources\normal\saut slime normal droit\saut_normal_droit_5.png'))
+
+        self.sprites1_left.append(pg.image.load('\resources\normal\saut slime normal gauche\saut_normal_gauche_0.png'))
+        self.sprites1_left.append(pg.image.load('\resources\normal\saut slime normal gauche\saut_normal_gauche_1.png'))
+        self.sprites1_left.append(pg.image.load('\resources\normal\saut slime normal gauche\saut_normal_gauche_2.png'))
+        self.sprites1_left.append(pg.image.load('\resources\normal\saut slime normal gauche\saut_normal_gauche_3.png'))
+        self.sprites1_left.append(pg.image.load('\resources\normal\saut slime normal gauche\saut_normal_gauche_4.png'))
+        self.sprites1_left.append(pg.image.load('\resources\normal\saut slime normal gauche\saut_normal_gauche_5.png'))
+
+        #SPRITE 2
+        self.sprites2_right.append(pg.image.load('\resources\stade1\saut slime stade 1 droit\saut_stade1_droit_0.png'))
+        self.sprites2_right.append(pg.image.load('\resources\stade1\saut slime stade 1 droit\saut_stade1_droit_1.png'))
+        self.sprites2_right.append(pg.image.load('\resources\stade1\saut slime stade 1 droit\saut_stade1_droit_2.png'))
+        self.sprites2_right.append(pg.image.load('\resources\stade1\saut slime stade 1 droit\saut_stade1_droit_3.png'))
+        self.sprites2_right.append(pg.image.load('\resources\stade1\saut slime stade 1 droit\saut_stade1_droit_4.png'))
+        self.sprites2_right.append(pg.image.load('\resources\stade1\saut slime stade 1 droit\saut_stade1_droit_5.png'))
+
+        self.sprites2_left.append(pg.image.load('\resources\stade1\saut slime stade 1 gauche\saut_stade1_gauche_0.png'))
+        self.sprites2_left.append(pg.image.load('\resources\stade1\saut slime stade 1 gauche\saut_stade1_gauche_1.png'))
+        self.sprites2_left.append(pg.image.load('\resources\stade1\saut slime stade 1 gauche\saut_stade1_gauche_2.png'))
+        self.sprites2_left.append(pg.image.load('\resources\stade1\saut slime stade 1 gauche\saut_stade1_gauche_3.png'))
+        self.sprites2_left.append(pg.image.load('\resources\stade1\saut slime stade 1 gauche\saut_stade1_gauche_4.png'))
+        self.sprites2_left.append(pg.image.load('\resources\stade1\saut slime stade 1 gauche\saut_stade1_gauche_5.png'))
+
+        #SPRITE 3
+        self.sprites3_right.append(pg.image.load('\resources\stade2\saut slime stade 2 droit\saut_stade2_droit_0.png'))
+        self.sprites3_right.append(pg.image.load('\resources\stade2\saut slime stade 2 droit\saut_stade2_droit_1.png'))
+        self.sprites3_right.append(pg.image.load('\resources\stade2\saut slime stade 2 droit\saut_stade2_droit_2.png'))
+        self.sprites3_right.append(pg.image.load('\resources\stade2\saut slime stade 2 droit\saut_stade2_droit_3.png'))
+        self.sprites3_right.append(pg.image.load('\resources\stade2\saut slime stade 2 droit\saut_stade2_droit_4.png'))
+        self.sprites3_right.append(pg.image.load('\resources\stade2\saut slime stade 2 droit\saut_stade2_droit_5.png'))
+
+        self.sprites3_left.append(pg.image.load('\resources\stade2\saut slime stade 2 gauche\saut_stade2_gauche_0.png'))
+        self.sprites3_left.append(pg.image.load('\resources\stade2\saut slime stade 2 gauche\saut_stade2_gauche_1.png'))
+        self.sprites3_left.append(pg.image.load('\resources\stade2\saut slime stade 2 gauche\saut_stade2_gauche_2.png'))
+        self.sprites3_left.append(pg.image.load('\resources\stade2\saut slime stade 2 gauche\saut_stade2_gauche_3.png'))
+        self.sprites3_left.append(pg.image.load('\resources\stade2\saut slime stade 2 gauche\saut_stade2_gauche_4.png'))
+        self.sprites3_left.append(pg.image.load('\resources\stade2\saut slime stade 2 gauche\saut_stade2_gauche_5.png'))
+
+        #SPRITE 4
+        self.sprites4.append(pg.image.load('\resources\mort\mort1.png'))
+        self.sprites4.append(pg.image.load('\resources\mort\mort2.png'))
+
+        #infos about blob sprites management
+        self.current_sprite = 0
+        if self.health == 3 :
+            self.image = self.sprites1[self.current_sprite]
+        if self.health == 2 :
+            self.image = self.sprites2[self.current_sprite]
+        if self.health == 1 :
+            self.image = self.sprites3[self.current_sprite]
+        if self.health == 0 :
+            self.image = self.sprites4[self.current_sprite]
+        self.rect = self.image.get_rect()
+
     
     #def jump(self):
         #if self.right:
@@ -50,7 +122,8 @@ class player_entity:
             self.left = True
             self.right = False
 
-        elif keys[pg.K_RIGHT] and self.x < 1130:
+
+        elif keys[pg.K_RIGHT] and self.x < 1100:
             self.x += self.vel
             self.left = False
             self.right = True
@@ -70,10 +143,7 @@ class player_entity:
 
     def moveup(self):
         keys = pg.key.get_pressed()
-        if self.vely > 720:
-            self.vely = 720
-
         if keys[pg.K_SPACE]:
-            self.vely = -20
+            self.vely = -30
 
 #End
