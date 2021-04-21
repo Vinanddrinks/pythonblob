f2f5bf7224b6d7fa99aad6b4b27eb07ac2fd840a
Djayos <joseph.beaumont1@gmail.com>
2021-04-22 00:19:29 +0200

    tried to put score but not working

--- functions.py
+++ functions.py
@@ -8,3 +8,18 @@ import numpy as n
 import time as t
 import random as r
 # End-importation
+
+# Variables
+score = 0
+ScoreLat = 0
+
+# End - Variable
+
+def SpreadTime():
+    global score
+    global ScoreLat
+    if t.time() - ScoreLat >= 0.1:
+        ScoreLat = t.time()
+        score += 1
+        print(score)
+    
--- main.py
+++ main.py
@@ -7,27 +7,38 @@ import pygame as pg
 from numpy import *
 import time as t
 from Classes import *
+from functions import *
 #end-importation
 
 
 # BEGIN
+
 #pygame initialisation
 pg.init()
 clock = pg.time.Clock()
 #end pg initialisation
+
+
 #declaration
 fond = pg.image.load('fond.jpg')
-#font = pg.font.SysFont('comicsans', 30, True)
-#text = font.render('Score : ', score, 1, (0, 0, 0))
+font = pg.font.SysFont('comicsans', 50, True)
+Markscore = font.render('Score : ' + str(score), 1, (0, 0, 255))
 keys = pg.key.get_pressed()
 window = pg.display.set_mode((1280, 720))
 pg.display.set_caption("Protect PyBlob") 
 imageperso = pg.image.load('perso jeu droite.png')
 blob = player_entity(40, 400)
 run = True
-score = 0
 testcovid = ennemy()
 #end_declaration
+
+# Def Update (don't touch !)
+def WindowUpdate():
+    window.blit(fond,(0,0))
+    window.blit(testcovid.sprite,(testcovid.x,testcovid.y))
+    window.blit(imageperso, (blob.x, blob.y))
+    window.blit(Markscore, (30, 30) )
+# End def Update
 
 
 while run == True:
@@ -40,9 +51,12 @@ while run == True:
     blob.moveup()
     testcovid.trajectory()
     print('blob x :',blob.x,' blob y:',blob.y)
-    window.blit(fond,(0,0))
-    window.blit(testcovid.sprite,(testcovid.x,testcovid.y))
-    window.blit(imageperso, (blob.x, blob.y))
+    SpreadTime()
+    print(score)
+    WindowUpdate()
     pg.display.update()
+    
+
 pg.quit()
 # END
+
