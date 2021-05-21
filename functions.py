ea3839674984205ab50a57d6906a3a6107292214
exo-nanas <81410359+exo-nanas@users.noreply.github.com>
2021-05-21 11:28:52 +0200

    changement musiques (3) : j'avais inversé stage 1 et 3

--- functions.py
+++ functions.py
@@ -12,13 +12,13 @@ import random as r
 # Variables
 def chooseSong(health):
     if health == 3:
-        pg.mixer.music.load('musiques/Dungeonboss.wav')
+        pg.mixer.music.load('musiques/Powerup.wav')
         pg.mixer.music.play(-1)
     elif health == 2:
         pg.mixer.music.load('musiques/Maze.wav')
         pg.mixer.music.play(-1)
     elif health == 1:
-        pg.mixer.music.load('musiques/Powerup.ogg')
+        pg.mixer.music.load('musiques/Dungeonboss.ogg')
         pg.mixer.music.play(-1)
     elif health == 0:
         pg.mixer.music.load('musiques/Virtualboy.wav')
